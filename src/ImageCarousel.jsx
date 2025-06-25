c51baddd5d88922a14ef2522723ccb9ccebb80fa
rozierj <128178808+rozierj@users.noreply.github.com>
2025-06-25 18:29:47 -0400

    Updates

---
+++ src/ImageCarousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageCarousel from './components/ImageCarousel'; // Adjust path if needed
+
+function App() {
+  return (
+    <div>
+      {/* Other sections */}
+      <ImageCarousel />
+      {/* More content */}
+    </div>
+  );
+}
+
+export default App;
