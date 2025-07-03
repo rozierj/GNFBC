a45295eb88487a3ed9d1d470150303521b010d83
rozierj <128178808+rozierj@users.noreply.github.com>
2025-07-02 21:13:32 -0400

    Adjusted mobile view to gallery

--- src/About.js
+++ src/About.js
@@ -5,7 +5,9 @@ export default function About() {
     <div className="p-8">
       <h1 className="text-4xl font-bold mb-4">About Us</h1>
       <p className="max-w-3xl">
-        Greater New Friendship Baptist Church is a community of believers dedicated to spreading the Gospel, serving our community, and growing together in faith. Our rich history, vibrant ministries, and dedication to Biblical teaching help us fulfill our mission to make disciples of all nations.
+        Members from Pearly Chapel and Mt. McCrae (Hatoff), two small, rural churches, joined together to form one congregation and to build a new sanctuary on the grounds of the Pearly Chapel site. 
+        On January 3, 1999, the first worship service was held in the newly organized Greater New Friendship Baptist Church located on Highway 19 South in Laurens County, Georgia. 
+        Since that time, three pastors have served, new ministries have been instituted, and members continue to work and serve together in love and harmony..
       </p>
     </div>
   );
